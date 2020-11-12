2ecc27673af68c7bb23105d53e5899ec40e0690a
Emilio04ca <cordovacarlos210@gmail.com>
2020-11-12 14:33:53 -0600

    WCF

---
+++ WCFServicio/WCFServicio/IService1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+            int ElvarNumeroAlCuadrado(int Numero);
+        
+        [OperationContract]
+            int RestaDeDosNumeros(int Numero1, int Numero2);
+        
+        [OperationContract]
+            Calificacion Obtenercalificacion(char letra);
+
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+    [DataContract]
+    public class Calificacion : resultado {
+        [DataMember]
+        public string Nombre { get; set; }
+
+        [DataMember]
+        public int Numerode_control { get; set; }
+        [DataMember]
+        public string Materia { get; set; }
+    }
+
+    [DataContract] 
+
+    public class resultado
+    { 
+        public string Fallo { get;set;}
+     }
+
+    
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WCFServicio/WCFServicio/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServicio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        //string nombre = "";
+        //Alumno alumno = new Alumno();
+
+        public Calificacion Obtenercalificacion(char letra)
+        {
+            if (letra == 'a' )
+            {
+                return new Calificacion() {Nombre = "Elizabeth", Materia = "medico forence", Numerode_control = 17030081 };
+            }
+            if (letra == 'b')
+            {
+                return new Calificacion() { Nombre = "Andrea", Materia = "administracion", Numerode_control = 17030083 };
+            }
+            else
+            {
+                return new Calificacion() { Fallo = "nigun Usuario con esa califiacion " };
+            }
+        }
+        public int RestaDeDosNumeros( int Numero1, int Numero2 )
+        {
+            return Numero1 - Numero2;
+
+        }
+
+        public int ElvarNumeroAlCuadrado(int Numero)
+        {
+            return Numero * Numero;
+        }
+        
+       
+    }
+}
